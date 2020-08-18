f4766b8f0b02a74d7500e87b486f8bef57f1933b
Ignacio <nachoabadie@gmail.com>
2020-08-18 15:43:59 -0300

    Agrego funcion solucion al problema

--- armstrongNumber.js
+++ armstrongNumber.js
@@ -4,3 +4,11 @@ Define a function that allows the user to check whether a given number is armstr
 Hint: To do this, first determine the number of digits of the given number. Call that n. Then take every digit in the number and raise it to the nth power. Add them, and if your answer is the original number then it is an Armstrong number.
 Example: Take 1634. Four digits. So, 1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634. So 1634 is an Armstrong number.
 Tip: All single digit numbers are Armstrong numbers */
+
+function armstrong (num) {
+    let digits = num.toString().split('');
+    let reduce = digits.reduce((count, digit) => count += Math.pow(digit, digits.length) ,0);
+    return num == reduce 
+        ?   "is Armstrong"
+        :   "is not Armstrong"
+}
